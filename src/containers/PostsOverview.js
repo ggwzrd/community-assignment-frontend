15ed78f07860c799b45d950982de807bc1a24099
brunadafonseca <brunadafonseca@gmail.com>
2018-01-29 17:57:49 +0100

    Create PostsOverview component

--- src/containers/PostsOverview.js
+++ src/containers/PostsOverview.js
@@ -1,1 +1,32 @@
-// renders PostItems and Tags
+// import React, { PureComponent } from 'react'
+// import { connect } from 'react-redux'
+// import PropTypes from 'prop-types'
+// import PostItem from './PostItem'
+// import Tag from './Tag'
+// import './styles/PostsOverview.css'
+// import { fetchPosts, fetchTags } from '../actions/......'
+//
+// class PostsOverview extends PureComponent {
+//   static propTypes = {
+//     posts: PropTypes.arrayOf(postShape).isRequired,
+//     tags: PropTypes.arrayOf(tagShape).isRequired
+//   }
+//
+//   componentWillMount() {
+//     this.props.fetchPosts()
+//   }
+//
+//   render() {
+//     return (
+//       <div className="posts-container">
+//         { this.props.posts.map(post => <PostItem />) }
+//       </div>
+//     )
+//   }
+// }
+//
+// const mapStateToProps = state => ({
+//   posts: state.posts
+// })
+//
+// export default connect(mapStateToProps, { fetchPosts, fetchTags })(PostsOverview)
