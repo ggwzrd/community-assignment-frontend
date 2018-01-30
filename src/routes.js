d7dc033f46fcbd30b92ac09c3f7134a33d38b451
brunadafonseca <brunadafonseca@gmail.com>
2018-01-30 22:37:23 +0100

    Add create posts feature

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom'
 import PostsOverview from './containers/PostsOverview'
 import PostPage from './containers/PostPage'
 import ProfilePage from './containers/users/ProfilePage'
+import CreatePostForm from './components/CreatePostForm'
 
 export default class Routes extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class Routes extends Component {
       <main className="mainContainer">
         <Route exact path="/" component={PostsOverview} />
         <Route exact path="/posts" component={PostsOverview} />
+        <Route exact path="/new-post" component={CreatePostForm} />
         <Route exact path="/posts/:postId" component={PostPage} />
         <Route exact path="/users/:userId" component={ProfilePage} />
       </main>
