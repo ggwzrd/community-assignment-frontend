972475ea0507445bc86feb347d61d9717810b332
fandytcc <fandytsui@gmail.com>
2018-01-31 15:55:02 +0100

    display tags

--- src/containers/PostsOverview.js
+++ src/containers/PostsOverview.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 // import PropTypes from 'prop-types'
 import PostItem from '../components/PostItem'
-
+import TagItem from '../components/TagItem'
 // import Tag from './Tag'
 import './styles/PostsOverview.css'
 import { fetchPosts } from '../actions/posts/fetch'
@@ -19,16 +19,23 @@ class PostsOverview extends PureComponent {
 
   render() {
     console.log(this.props)
+
+    let tagsArray = this.props.posts.map(post => post.tags)
+    console.log(tagsArray)
+    let mergedtagArray = [].concat.apply([],tagsArray)
+    console.log(mergedtagArray)
+    let tagIdArray = mergedtagArray.map(item => item.id)
+    console.log(tagIdArray)
+
     return (
       <div className="container">
         <div className="tags-container">
-          <p>Tags here</p>
-          <p>Tags here</p>
-          <p>Tags here</p>
-          <p>Tags here</p>
-          <p>Tags here</p>
-          <p>Tags here</p>
-          <p>Tags here</p>
+          {mergedtagArray.map(tag =>
+            <TagItem
+              name={tag.name}
+              total_mentions={tagIdArray.filter(id => id === tag.id).length}
+              description={tag.description}
+              />)}
         </div>
         <div className="posts-container">
           {this.props.posts.map(post =>
