a04bdbb7c7629ca3d2fcbb5de4d4c5b685f0868f
Giulio Gallerini <giulio.gallerini.work@hotmail.com>
2018-02-04 01:47:35 +0100

    move Dialog into PostPage and make sure that we scroll is based on full page

--- src/containers/PostPage.js
+++ src/containers/PostPage.js
@@ -1,40 +1,52 @@
-import React, { PureComponent, Fragment } from 'react'
-import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
-import { fetchOnePost, fetchSources } from '../actions/posts/fetch'
-import { reportPost } from '../actions/posts/report'
-import { trustPost } from '../actions/posts/trust'
-// import Paper from 'material-ui/Paper'
-// import Button from 'material-ui/Button'
+import React, { PureComponent, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+// material-ui
+import Dialog from 'material-ui/Dialog';
+import { withStyles, } from 'material-ui/styles';
 import Card, { CardHeader, CardContent, CardMedia } from 'material-ui/Card';
 import IconButton from 'material-ui/IconButton';
 import Typography from 'material-ui/Typography';
-// import SkipPreviousIcon from 'material-ui-icons/SkipPrevious';
-// import PlayArrowIcon from 'material-ui-icons/PlayArrow';
-// import SkipNextIcon from 'material-ui-icons/SkipNext';
 import Avatar from 'material-ui/Avatar'
 import Badge from 'material-ui/Badge';
 import VerifiedUserIcon from 'material-ui-icons/VerifiedUser';
 import ReportIcon from 'material-ui-icons/Report';
 import Tooltip from 'material-ui/Tooltip';
-// import Dialog, {
-//   DialogActions,
-//   DialogContent,
-//   DialogContentText,
-//   DialogTitle,
-// } from 'material-ui/Dialog'
-// import TextField from 'material-ui/TextField'
-// import Radio, { RadioGroup } from 'material-ui/Radio'
-// import { FormLabel, FormControl, FormControlLabel } from 'material-ui/Form'
-import ReportForm from '../components/forms/ReportForm'
-import TrustForm from '../components/forms/TrustForm'
 
-// import facebook from './images/sources/facebook.svg'
-// import google from './images/sources/google.jpg'
-// import coinerd from './images/sources/logo.svg'
-// import reddit from './images/sources/reddit.svg'
-// import twitter from './images/sources/twitter.svg'
-import './styles/PostPage.css'
+// components
+import ReportForm from '../components/forms/ReportForm';
+import TrustForm from '../components/forms/TrustForm';
+import Comment from '../components/Comment';
+
+// actions
+import { fetchOnePost, fetchSources } from '../actions/posts/fetch';
+import { reportPost } from '../actions/posts/report';
+import { trustPost } from '../actions/posts/trust';
+
+// selectors
+import { selectComments, } from '../selectors/select-comments';
+
+// styles
+import './styles/PostPage.css';
+
+const styles = {
+  root: {
+    overflow: 'scroll',
+    maxHeight: '100vh',
+  },
+  paper: {
+    display: 'block',
+    overflow: 'visible',
+    maxHeight: 'none',
+    // backgroundColor: 'transparent',
+  },
+  subheading: {
+    color: '#cccccc',
+    marginLeft: '16px',
+    borderBottom: '1px solid #cccccc',
+  }
+};
 
 export const postShape = PropTypes.shape({
   id: PropTypes.string.isRequired,
@@ -44,7 +56,13 @@ export const postShape = PropTypes.shape({
   is_spam: PropTypes.bool,
   // user: PropTypes.arrayOf(userShape),
   trusts: PropTypes.array,
-  reports: PropTypes.array
+  reports: PropTypes.array,
+  comments: PropTypes.arrayOf(PropTypes.shape({
+    avatar: PropTypes.string,
+    content: PropTypes.object,
+    source: PropTypes.object,
+    type: PropTypes.string,
+  })).isRequired,
 })
 
 class PostPage extends PureComponent {
@@ -52,8 +70,16 @@ class PostPage extends PureComponent {
     ...postShape.isRequired,
   }
 
-  componentWillMount() {
-    this.props.fetchOnePost(this.props.postId)
+  constructor(props, context) {
+    super(props, context);
+
+    this.handlePostClose = this.handlePostClose.bind(this);
+  }
+
+  componentDidMount() {
+    const { params, } = this.props.match;
+
+    this.props.fetchOnePost(params.postId)
     this.props.fetchSources()
   }
 
@@ -70,6 +96,12 @@ class PostPage extends PureComponent {
 
   handleClose = () => {
     this.setState({ open: false })
+  }
+
+  handlePostClose() {
+    const { history, } = this.props;
+
+    history.push('/posts');
   }
 
   setTrustState = () => {
@@ -118,9 +150,8 @@ class PostPage extends PureComponent {
 
 
   render() {
-    if (!!this.props.selectedPost) {
-      var { content, trusts, reports, images, created_at } = this.props.selectedPost
-    }
+    const { classes, } = this.props;
+    const { content, trusts, reports, images, created_at, comments,  } = this.props;
 
     const date = new Date(created_at).toLocaleString("UTC", { hour12: false,
                                                              year:   'numeric',
@@ -128,86 +159,114 @@ class PostPage extends PureComponent {
                                                              day:    'numeric',
                                                              hour:   'numeric',
                                                              minute: 'numeric' })
-
     return (
-      <Card className="expanded-post-item"  elevation={0}>
-        <CardMedia
-          className="expanded-cover"
-          image={images}
-          />
-        <div className="expanded-details">
+      <Dialog
+        open
+        onClose={this.handlePostClose}
+        aria-labelledby="form-dialog-title"
+        classes={{
+          root: classes.root,
+          paper: classes.paper,
+        }}
+      >
+        <Card className="expanded-post-item"  elevation={0}>
+          <CardMedia
+            className="expanded-cover"
+            image={images}
+            />
+          <div className="expanded-details">
 
-          <div className="formwrapper">
-            {this.state.reportFormIsOpen ? <ReportForm
-                                            handleChange={this.handleChange}
-                                            setReportState={this.setReportState}
-                                            handleReportClick={this.handleReportClick}/> : null}
+            <div className="formwrapper">
+              {this.state.reportFormIsOpen ? <ReportForm
+                                              handleChange={this.handleChange}
+                                              setReportState={this.setReportState}
+                                              handleReportClick={this.handleReportClick}/> : null}
 
-            {this.state.trustFormIsOpen ? <TrustForm
-                                            handleChange={this.handleChange}
-                                            setReportState={this.setTrustState}
-                                            handleReportClick={this.handleTrustClick}
-                                            sources={this.props.sources}
-                                            sourceIdState={this.state.source_id}/> : null}
+              {this.state.trustFormIsOpen ? <TrustForm
+                                              handleChange={this.handleChange}
+                                              setReportState={this.setTrustState}
+                                              handleReportClick={this.handleTrustClick}
+                                              sources={this.props.sources}
+                                              sourceIdState={this.state.source_id}/> : null}
+            </div>
+
+
+
+            <CardHeader className="expanded-card-header"
+              avatar={
+                <Badge className="expanded-badge" badgeContent={100} color="default">
+                <Avatar
+                  alt="Remy Sharp"
+                  src="https://cdn2.f-cdn.com/files/download/24619452/natural+background.png"
+                  />
+                </Badge>
+
+              }
+              action={
+                <Fragment>
+                <IconButton onClick={this.setTrustState}>
+                  <Tooltip id="tooltip-top" title="Trust this post" placement="top" className="tooltip">
+                    <Badge className="badge trust" badgeContent={!!trusts ? trusts.length : 0} color="default">
+                      <VerifiedUserIcon fontSize="true"/>
+                    </Badge>
+                </Tooltip>
+
+                </IconButton>
+                <IconButton onClick={this.setReportState}>
+                  <Tooltip id="tooltip-top" title="Report this post" placement="top" className="tooltip">
+                    <Badge className="badge report" badgeContent={!!reports ? reports.length : 0} color="default">
+                      <ReportIcon fontSize="true" className="badgeIcon"/>
+                    </Badge>
+                </Tooltip>
+
+                </IconButton>
+              </Fragment>
+              }
+              title="Name Lastname"
+              subheader={date}
+            />
+            <CardContent className="expanded-content">
+
+              <Typography type="body1" >
+                {content}
+              </Typography>
+            </CardContent>
           </div>
-
-
-
-          <CardHeader className="expanded-card-header"
-            avatar={
-              <Badge className="expanded-badge" badgeContent={100} color="default">
-              <Avatar
-                alt="Remy Sharp"
-                src="https://cdn2.f-cdn.com/files/download/24619452/natural+background.png"
-                />
-              </Badge>
-
-            }
-            action={
-              <Fragment>
-              <IconButton onClick={this.setTrustState}>
-                <Tooltip id="tooltip-top" title="Trust this post" placement="top" className="tooltip">
-                  <Badge className="badge trust" badgeContent={!!trusts ? trusts.length : 0} color="default">
-                    <VerifiedUserIcon fontSize="true"/>
-                  </Badge>
-              </Tooltip>
-
-              </IconButton>
-              <IconButton onClick={this.setReportState}>
-                <Tooltip id="tooltip-top" title="Report this post" placement="top" className="tooltip">
-                  <Badge className="badge report" badgeContent={!!reports ? reports.length : 0} color="default">
-                    <ReportIcon fontSize="true" className="badgeIcon"/>
-                  </Badge>
-              </Tooltip>
-
-              </IconButton>
-            </Fragment>
-            }
-            title="Name Lastname"
-            subheader={date}
-          />
-          <CardContent className="expanded-content">
-
-            <Typography type="body1" >
-              {content}
-            </Typography>
-          </CardContent>
-        </div>
-      </Card>
-
-
-
+        </Card>
+        <Fragment>
+          <Typography
+            type="subheading"
+            gutterBottom
+            classes={{ subheading: classes.subheading, }}>Comments</Typography>
+          {comments.map((comment, key) => (
+            <Comment key={key} {...comment} />
+          ))}
+        </Fragment>
+      </Dialog>
     )
   }
 }
 
+PostPage.defaultProps = {
+  content: 'lorem ipsum',
+  trusts: [],
+  reports: [],
+  images: '',
+  created_at: new Date(),
+}
+
 const mapStateToProps = state => ({
-  selectedPost: state.posts.selectedPost,
+  content: state.posts.selectedPost.content,
+  comments: selectComments(state),
+  trusts: state.posts.selectedPost.trusts,
+  reports: state.posts.selectedPost.reports,
+  images: state.posts.selectedPost.images,
+  created_at: state.posts.selectedPost.created_at,
   sources: state.sources,
-  loading: state.loading
+  loading: state.loading,
 })
 
-export default connect(mapStateToProps, { fetchOnePost, fetchSources, reportPost, trustPost })(PostPage)
+export default connect(mapStateToProps, { fetchOnePost, fetchSources, reportPost, trustPost })(withStyles(styles)(PostPage))
 
 
 
