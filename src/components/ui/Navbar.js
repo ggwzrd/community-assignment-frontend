a9de680e2716ed4fba8e76b0d6fd556a9df7ce08
fandytcc <fandytsui@gmail.com>
2018-02-07 10:19:07 +0100

    clean up

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -13,7 +13,6 @@ import Typography from 'material-ui/Typography'
 import Menu, { MenuItem } from 'material-ui/Menu'
 import Avatar from 'material-ui/Avatar'
 import Dialog from 'material-ui/Dialog'
-import IconButton from 'material-ui/IconButton'
 import './Navbar.css'
 
 class Navbar extends React.Component {
@@ -84,143 +83,6 @@ class Navbar extends React.Component {
     this.setState({ anchorEl: null })
   }
 
-//sign-in
-  updateEmail(event) {
-    this.setState({
-      email: event.target.value
-    })
- }
-
-  updatePassword(event) {
-    this.setState({
-      password: event.target.value
-    })
-  }
-
-  submitSignInForm(event) {
-    event.preventDefault()
-    const user = {
-      user: { email: this.state.email,
-              password: this.state.password
-            }
-    }
-    this.props.signIn( user )
-
-    this.handleDialogClose()
-  }
-
-//sign-up
-  validateAll() {
-    return this.validateNickname() &&
-      this.validateEmail() &&
-      this.validatePassword() &&
-      this.validatePasswordConfirmation()
-  }
-
-  submitSignUpForm(event) {
-    event.preventDefault()
-    if (this.validateAll()) {
-      const user = {
-        first_name: this.state.first_name,
-        last_name: this.state.last_name,
-        nickname: this.refs.nickname.getValue(),
-        email: this.refs.email.getValue(),
-        password: this.refs.password.getValue()
-      }
-      this.props.signUp(user)
-    }
-    this.handleDialogClose()
-  }
-
-  setFirstName(event) {
-    this.setState({
-      first_name: event.target.value
-    })
-  }
-
-  setLastName(event) {
-    this.setState({
-      last_name: event.target.value
-    })
-  }
-
-  validateNickname() {
-    const { nickname } = this.refs
-    if (nickname.getValue().length > 1) {
-      this.setState({
-        nicknameError: null
-      })
-      return true
-    }
-
-    this.setState({
-      nicknameError: 'Please provide your nickname'
-    })
-    return false
-  }
-
-  validateEmail() {
-    const { email } = this.refs
-
-    if (email.getValue().match(/^[a-z0-9._-]+@[a-z0-9._-]+.[a-z0-9._-]+$/)) {
-      this.setState({
-        emailError: null
-      })
-      return true
-    }
-
-    if (email.value === '') {
-      this.setState({
-        emailError: 'Please provide your email address'
-      })
-      return false
-    }
-
-    this.setState({
-      emailError: 'Please provide a valid email address'
-    })
-    return false
-  }
-
-  validatePassword() {
-    const { password } = this.refs
-
-    if (password.getValue().length < 6) {
-      this.setState({
-        passwordError: 'Password is too short'
-      })
-      return false
-    }
-
-    if (password.getValue().match(/[a-zA-Z]+/) && password.getValue().match(/[0-9]+/)) {
-      this.setState({
-        passwordError: null
-      })
-      return true
-    }
-
-    this.setState({
-      passwordError: 'Password should contain both letters and numbers'
-    })
-    return false
-  }
-
-  validatePasswordConfirmation() {
-    const { password, passwordConfirmation } = this.refs
-
-    if (password.value === passwordConfirmation.value) {
-      this.setState({
-        passwordConfirmationError: null
-      })
-      return true
-    }
-
-    this.setState({
-      passwordConfirmationError: 'Passwords do not match'
-    })
-    return false
-  }
-
   renderFullname = () => {
     return this.props.user === undefined
       ? null
@@ -275,43 +137,25 @@ class Navbar extends React.Component {
 
         <div className="formwrapper">
           <div>
-          <Dialog
-            open={this.state.signUpFormIsOpen}
-            onClose={this.handleDialogClose}
-            aria-labelledby="signUp-form-dialog">
-              <SignUpForm
-                handleDialogClose={this.handleDialogClose.bind(this)}/>
-          </Dialog>
+            <Dialog
+              open={this.state.signUpFormIsOpen}
+              onClose={this.handleDialogClose}
+              aria-labelledby="signUp-form-dialog">
+                <SignUpForm
+                  handleDialogClose={this.handleDialogClose.bind(this)}/>
+            </Dialog>
           </div>
 
           <div>
-              <Dialog
-                open={this.state.signInFormIsOpen}
-                onClose={this.handleDialogClose}
-                aria-labelledby="signIn-form-dialog">
-              <SignInForm
-                handleDialogClose={this.handleDialogClose.bind(this)} />
-              </Dialog>
+            <Dialog
+              open={this.state.signInFormIsOpen}
+              onClose={this.handleDialogClose}
+              aria-labelledby="signIn-form-dialog">
+            <SignInForm
+              handleDialogClose={this.handleDialogClose.bind(this)} />
+            </Dialog>
           </div>
         </div>
-                handleDialogClose={this.handleDialogClose}
-                submitSignUpForm={this.submitSignUpForm.bind(this)}
-                validateNickname={this.validateNickname.bind(this)}
-                validateEmail={this.validateEmail.bind(this)}
-                validatePassword={this.validatePassword.bind(this)}
-                validatePasswordConfirmation={this.validatePasswordConfirmation.bind(this)}/>
-          </Dialog>
-
-          <Dialog
-            open={this.state.signInFormIsOpen}
-            onClose={this.handleDialogClose}
-            aria-labelledby="signIn-form-dialog">
-              <SignInForm
-                handleDialogClose={this.handleDialogClose}
-                submitSignInForm={this.submitSignInForm.bind(this)}
-                updatePassword={this.updatePassword.bind(this)}
-                updateEmail={this.updateEmail.bind(this)} />
-          </Dialog>
       </div>
     )
   }
